e6e0c0e42874d4d3bdc5810a80be0fa383cae1cc
Arturo Quiros <adquirose@gmail.com>
2023-09-06 14:30:45 -0300

    borrado seccion multiuso

---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+export const data = [
+    {
+      id: 1,
+      titulo: "SITIOS EN PUCÓN DESDE 600 M2",
+      contenido:
+        "Cotiza con nosotros. Contamos con instalaciones únicas como un portal de acceso con vigilancia controlada, quincho, sala de juegos, multicancha y áreas de esparcimiento, caminos interiores asfaltados, luz, canalización subterránea, agua, alcantarillado e internet fibra óptica.",
+      image: require("../assets/img/proyecto/texto1.jpg"),
+      altImage: "imagen1",
+      reverse: false,
+    },
+    {
+      id: 2,
+      titulo: "ARMONÍA CON LA NATURALEZA",
+      contenido:
+        "El condominio urbano Altos del Quelhue está ubicado dentro de la Hacienda Puerto Pucón, en un terreno que cuenta con vistas insuperables del Volcán Villarrica y el imponente Cerro de Quelhue.",
+      image: require("../assets/img/proyecto/texto2.jpg"),
+      altImage: "imagen2",
+      reverse: true,
+    },
+    {
+      id: 3,
+      titulo: "ESPECTACULAR ACCESO",
+      contenido:
+        "Ubicado a metros del Gimnasio Olímpico emplazado la Ciudad del Deporte Ecoparque de Pucón; el condominio cuenta con dos accesos: uno por la hacienda Puerto Pucón, en el Camino Internacional, y otro pavimentado por camino al Ecoparque, que conecta rápidamente -a sólo 5 minutos- con el centro urbano de Pucón.",
+      image: require("../assets/img/proyecto/texto3.jpg"),
+      altImage: "imagen3",
+      reverse: false,
+    }
+  ];
